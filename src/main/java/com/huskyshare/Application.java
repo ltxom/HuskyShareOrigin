58281f6e259fcdc528a6b0a22461f7c03298c514
LTXOM <ltxom@uw.edu>
2018-10-18 15:03:59 -0700

    Revert "Removed Maven (Yeah!!)"
    This reverts commit 81181d6269e966d88d3758a16ea47bdb8328879b.

---
+++ src/main/java/com/huskyshare/Application.java
@@ -0,0 +1,7 @@
+package com.huskyshare;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+}
---
+++ src/main/java/com/huskyshare/beans/UploadFile.java
@@ -0,0 +1,12 @@
+package com.huskyshare.beans;
+
+public class UploadFile {
+	private int id;
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getId() {
+		return id;
+	}
+}
